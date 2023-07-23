9edaeb927cc278f1515aa0f439b0bcff1a76c18c
Ivaylo Ganchev <ganchev@Ivaylos-MacBook-Air.local>
2023-07-23 18:45:33 +0300

    Minor adjustments to footer and home page

--- wp-content/themes/zoohotel/footer.php
+++ wp-content/themes/zoohotel/footer.php
@@ -8,13 +8,6 @@
             <li class="social-icon__item"><a class="social-icon__link" href="#"><ion-icon name="logo-linkedin"></ion-icon></a></li>
             <li class="social-icon__item"><a class="social-icon__link" href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
         </ul>
-        <ul class="menu">
-            <li class="menu__item"><a class="menu__link" href="#">Home</a></li>
-            <li class="menu__item"><a class="menu__link" href="#">About</a></li>
-            <li class="menu__item"><a class="menu__link" href="#">Services</a></li>
-            <li class="menu__item"><a class="menu__link" href="#">Team</a></li>
-            <li class="menu__item"><a class="menu__link" href="#">Contact</a></li>
-        </ul>
-        <p class="footer-trademark">&copy;<?php echo date("Y"); ?> Zoo Hotel Viki | All Rights Reserved</p>
+        <p class="footer-trademark">&copy;<?php echo date("Y"); ?> Pet Hotel Viki | All Rights Reserved</p>
     </footer>
 </div>
