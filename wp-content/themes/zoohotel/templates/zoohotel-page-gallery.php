cee4f0bcf2aa03727b71f4df8bd212fc901ff9d5
Ivaylo Ganchev <ganchev@Ivaylos-MacBook-Air.local>
2023-07-25 13:03:51 +0300

    Added gallery plugin

--- wp-content/themes/zoohotel/templates/zoohotel-page-gallery.php
+++ wp-content/themes/zoohotel/templates/zoohotel-page-gallery.php
@@ -7,55 +7,10 @@
 get_header();
 ?>
 
-<!-- PAGE TEMPLATE STARTS HERE -->
-<div class="container zoo-hotel-gallery-page">
-  <!-- Gallery -->
-        <!--Section description-->
-  <h2 class="h1-responsive font-weight-bold text-center my-4">Галерия</h2>
-  <div class="row">
-    <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-      <img
-        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-        class="w-100 shadow-1-strong rounded mb-4"
-        alt="Boat on Calm Water"
-      />
-
-      <img
-        src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
-        class="w-100 shadow-1-strong rounded mb-4"
-        alt="Wintry Mountain Landscape"
-      />
-    </div>
-
-    <div class="col-lg-4 mb-4 mb-lg-0">
-      <img
-        src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
-        class="w-100 shadow-1-strong rounded mb-4"
-        alt="Mountains in the Clouds"
-      />
-
-      <img
-        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-        class="w-100 shadow-1-strong rounded mb-4"
-        alt="Boat on Calm Water"
-      />
-    </div>
-
-    <div class="col-lg-4 mb-4 mb-lg-0">
-      <img
-        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
-        class="w-100 shadow-1-strong rounded mb-4"
-        alt="Waves at Sea"
-      />
-
-      <img
-        src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
-        class="w-100 shadow-1-strong rounded mb-4"
-        alt="Yosemite National Park"
-      />
-    </div>
-  </div>
-  <!-- Gallery -->
-</div>
+<?php echo do_shortcode('
+[modula id="19"]
+'
+);
+?>
 
 <?php get_footer();
