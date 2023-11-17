a9e1b3b7717810e887773200c9ba2a5dbc9c48f5
Ivaylo <ivaylo94@gmail.com>
2023-11-17 15:04:32 +0200

    New home page design

--- wp-content/themes/zoohotel/home.php
+++ wp-content/themes/zoohotel/home.php
@@ -1,9 +1,65 @@
 <?php get_header(); ?>
 
+<div class="container-fluid zoohotel-faq-page">
+  <div class="content-wrap shadow p-3 mb-5 rounded">
+
+    <section>
+
+      <div class="faq-wrapper"> 
+      <div class="row mb-4">
+          <div class="text-container">
+              <div class="dog-image">
+                <img src="<?php echo wp_upload_dir()["baseurl"]?>/media/square_dog.jpeg"/>
+              </div>
+              <div class="text-column">
+                <h6 class="mb-3 text-primary home-header"><i class="fa-solid fa-house"></i> НАСТАНЯВАНЕ </h6>
+                <p>Хотел за кучета „VIKI” предлага на своите четириноги гости различни места за настаняване според техните нужди и особености. Разполагаме със самостоятелни боксове. Всички помещения за спане са на закрито, в сгради, в които се поддържат адекватна за сезона температура и проветряване. Всички помещения, легълца и купички за храна се дезинфекцират ежедневно.</p>
+              </div>
+          </div>
+      </div>
+      <div class="row mb-4">
+            <div class="text-container">
+              <div class="text-column">
+                <h6 class="mb-3 text-primary home-header"><i class="fa-solid fa-bone"></i> ХРАНЕНЕ </h6>
+                <p>Храненето на Вашия любимец се извършва два пъти на ден. Можете да донесете храната, която кучето Ви яде по принцип, или да се възползвате от храната, предлагана в хотела. Работим с премиум клас храните на Eukanuba</p>
+            </div>
+            <div class="dog-image">
+              <img src="<?php echo wp_upload_dir()["baseurl"]?>/media/dog_eating_icecream.jpeg"/>
+            </div>
+          </div>
+      </div>
+      <div class="row mb-4">
+            <div class="text-container">
+              <div class="dog-image">
+                <img src="<?php echo wp_upload_dir()["baseurl"]?>/media/dog_with_ball.jpg"/>
+              </div>
+              <div class="text-column">
+                <h6 class="mb-3 text-primary home-header"><i class="fa-solid fa-football"></i> ЗАБАВЛЕНИЯ</h6>
+                <p>На разположение на всички наши гости е изцяло ограден комплекс от 2000 м2., разделен на изолирани едно от друго дворни пространства и сгради. Кучетата се разпределят в различните места за игра според размера си и степента на социалност. Кучетата под 10кг. не се събират с по-едри кучета, освен при изрично съгласие от стопанина . Имаме всякакви видове играчки, както и  дървета за излежаване на сянка. Ако времето го позволява. Когато са навън, кучетата са под непрекъснато наблюдение от човек. </p>
+              </div>
+          </div>
+      </div>
+      <div class="row mb-4">
+            <div class="text-container">
+              <div class="text-column">
+                <h6 class="mb-3 text-primary home-header"><i class="fas fa-exclamation-triangle"></i> ВАЖНО!</h6>
+                <p>Всяко куче, което е настанено в хотел "Viki", трябва да има предоставен паспорт с всички задължителни имунизации и обезпаразитявания, както и с поставена ваксина срещу Кучешка кашлица.Хотелът работи с “Ветеринарна амбулатория Ветрен” предоставящи грижа за Вашия любимец при нужда във всеки момент от денонощието. Хотелът има също “Сертификат за хуманно отношение и защита на животни, отглеждани в хотели”.</p>
+             </div>
+            <div class="dog-image">
+              <img src="<?php echo wp_upload_dir()["baseurl"]?>/media/dog_eating_icecream.jpeg"/>
+            </div>
+          </div>
+      </div>
+    </section>
+    <!--Section: FAQ-->
+  </div>
+</div>
+
+<!--
 <div class="container zoohotel-home-page">
 
   <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-    <!-- Card 1 -->
+     Card 1 
     <div class="col-lg-6 col-xxl-4 mb-5">
       <div class="card border-0 h-100">
         <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
@@ -17,7 +73,7 @@
         </div>
       </div>
     </div>
-    <!-- Card 2 -->
+     Card 2 
     <div class="col-lg-6 col-xxl-4 mb-5">
       <div class="card border-0 h-100">
         <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
@@ -32,7 +88,7 @@
         </div>
       </div>
     </div>
-    <!-- Card 3 -->
+     Card 3 
     <div class="col-lg-6 col-xxl-4 mb-5">
       <div class="card border-0 h-100">
         <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
@@ -62,7 +118,7 @@
         </div>
       </div>
     </div>
-    <!-- Card 5 -->
+     Card 5 
     <div class="col-lg-6 col-xxl-4 mb-5">
       <div class="card border-0 h-100">
         <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
@@ -76,7 +132,7 @@
         </div>
       </div>
     </div>
-    <!-- Card 6 -->
+     Card 6 
     <div class="col-lg-6 col-xxl-4 mb-5">
       <div class="card border-0 h-100">
         <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
@@ -93,6 +149,7 @@
     </div>
   </div>
 </div>
+-->
 <link rel="preconnect" href="https://fonts.googleapis.com"> 
 
 <link href='https://fonts.googleapis.com/css?family=Amatic SC' rel='stylesheet'>
