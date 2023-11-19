c45745a1ca3298308d32770acd6461241250a499
Ivaylo <ivaylo94@gmail.com>
2023-11-19 12:59:38 +0200

    Home page style and mobile compatability improvements

--- wp-content/themes/zoohotel/home.php
+++ wp-content/themes/zoohotel/home.php
@@ -6,8 +6,7 @@
     <section>
 
       <div class="faq-wrapper"> 
-      <div class="row mb-4">
-          <div class="text-container">
+      <div class="row mb-4 text-container" >
               <div class="dog-image">
                 <img src="<?php echo wp_upload_dir()["baseurl"]?>/media/square_dog.jpeg"/>
               </div>
@@ -15,10 +14,8 @@
                 <h6 class="mb-3 text-primary home-header"><i class="fa-solid fa-house"></i> НАСТАНЯВАНЕ </h6>
                 <p>Хотел за кучета „VIKI” предлага на своите четириноги гости различни места за настаняване според техните нужди и особености. Разполагаме със самостоятелни боксове. Всички помещения за спане са на закрито, в сгради, в които се поддържат адекватна за сезона температура и проветряване. Всички помещения, легълца и купички за храна се дезинфекцират ежедневно.</p>
               </div>
-          </div>
       </div>
-      <div class="row mb-4">
-            <div class="text-container">
+      <div class="row mb-4 text-container">
               <div class="text-column">
                 <h6 class="mb-3 text-primary home-header"><i class="fa-solid fa-bone"></i> ХРАНЕНЕ </h6>
                 <p>Храненето на Вашия любимец се извършва два пъти на ден. Можете да донесете храната, която кучето Ви яде по принцип, или да се възползвате от храната, предлагана в хотела. Работим с премиум клас храните на Eukanuba</p>
@@ -26,10 +23,8 @@
             <div class="dog-image">
               <img src="<?php echo wp_upload_dir()["baseurl"]?>/media/dog_eating_icecream.jpeg"/>
             </div>
-          </div>
       </div>
-      <div class="row mb-4">
-            <div class="text-container">
+      <div class="row mb-4 text-container">
               <div class="dog-image">
                 <img src="<?php echo wp_upload_dir()["baseurl"]?>/media/dog_with_ball.jpg"/>
               </div>
@@ -37,10 +32,8 @@
                 <h6 class="mb-3 text-primary home-header"><i class="fa-solid fa-football"></i> ЗАБАВЛЕНИЯ</h6>
                 <p>На разположение на всички наши гости е изцяло ограден комплекс от 2000 м2., разделен на изолирани едно от друго дворни пространства и сгради. Кучетата се разпределят в различните места за игра според размера си и степента на социалност. Кучетата под 10кг. не се събират с по-едри кучета, освен при изрично съгласие от стопанина . Имаме всякакви видове играчки, както и  дървета за излежаване на сянка. Ако времето го позволява. Когато са навън, кучетата са под непрекъснато наблюдение от човек. </p>
               </div>
-          </div>
       </div>
-      <div class="row mb-4">
-            <div class="text-container">
+      <div class="row mb-4 text-container">
               <div class="text-column">
                 <h6 class="mb-3 text-primary home-header"><i class="fas fa-exclamation-triangle"></i> ВАЖНО!</h6>
                 <p>Всяко куче, което е настанено в хотел "Viki", трябва да има предоставен паспорт с всички задължителни имунизации и обезпаразитявания, както и с поставена ваксина срещу Кучешка кашлица.Хотелът работи с “Ветеринарна амбулатория Ветрен” предоставящи грижа за Вашия любимец при нужда във всеки момент от денонощието. Хотелът има също “Сертификат за хуманно отношение и защита на животни, отглеждани в хотели”.</p>
@@ -48,7 +41,6 @@
             <div class="dog-image">
               <img src="<?php echo wp_upload_dir()["baseurl"]?>/media/dogs_paying_attention.jpeg"/>
             </div>
-          </div>
       </div>
     </section>
     <!--Section: FAQ-->
