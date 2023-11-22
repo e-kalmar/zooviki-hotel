761b25772cc98ebf9711f1139a14ebd049536bf2
Ivaylo <ivaylo94@gmail.com>
2023-11-22 09:49:13 +0200

    fixed gallery id

--- wp-content/themes/zoohotel/templates/zoohotel-page-gallery.php
+++ wp-content/themes/zoohotel/templates/zoohotel-page-gallery.php
@@ -9,7 +9,7 @@ get_header();
 
 <div class="container-fluid zoohotel-gallery-page">
     <div class="content-wrap shadow p-3 mb-5 rounded p-4">
-        <?php echo do_shortcode('[modula id="19"]'); ?>
+        <?php echo do_shortcode('[modula id="53"]'); ?>
     </div>
 </div>
 <link rel="preconnect" href="https://fonts.googleapis.com">
