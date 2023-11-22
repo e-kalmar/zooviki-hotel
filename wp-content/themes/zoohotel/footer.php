0718299a55734a73a235d6172671cc7987784c48
Ivaylo <ivaylo94@gmail.com>
2023-11-22 15:52:02 +0200

    Updated instagram link

--- wp-content/themes/zoohotel/footer.php
+++ wp-content/themes/zoohotel/footer.php
@@ -5,7 +5,7 @@
         <ul class="social-icon">
             <li class="social-icon__item"><a class="social-icon__link" href="https://www.facebook.com/ZooViki/" target="_blank"><ion-icon name="logo-facebook"></ion-icon></a></li>
             <!-- <li class="social-icon__item"><a class="social-icon__link" href="#"><ion-icon name="logo-twitter"></ion-icon></a></li> -->
-            <li class="social-icon__item"><a class="social-icon__link" href="https://www.instagram.com/zooviki.eu/" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a></li>
+            <li class="social-icon__item"><a class="social-icon__link" href="https://www.instagram.com/zoohotelviki/" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a></li>
             <li class="social-icon__item"><a class="social-icon__link" href="https://zooviki.eu/" target="_blank"><ion-icon name="link-outline"></ion-icon></a></li>
         </ul>
         <p class="footer-trademark">&copy;<?php echo date("Y"); ?> Pet Hotel Viki | All Rights Reserved</p>
