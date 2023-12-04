bfa9d40610822c5e9466965ad59614947a80d649
Ivaylo <ivaylo94@gmail.com>
2023-12-04 13:48:14 +0200

    Facebook page link updated

--- wp-content/themes/zoohotel/footer.php
+++ wp-content/themes/zoohotel/footer.php
@@ -3,7 +3,7 @@
 <div class="footer">
     <footer>
         <ul class="social-icon">
-            <li class="social-icon__item"><a class="social-icon__link" href="https://www.facebook.com/ZooViki/" target="_blank"><ion-icon name="logo-facebook"></ion-icon></a></li>
+            <li class="social-icon__item"><a class="social-icon__link" href="https://www.facebook.com/profile.php?id=61553938689151" target="_blank"><ion-icon name="logo-facebook"></ion-icon></a></li>
             <!-- <li class="social-icon__item"><a class="social-icon__link" href="#"><ion-icon name="logo-twitter"></ion-icon></a></li> -->
             <li class="social-icon__item"><a class="social-icon__link" href="https://www.instagram.com/zoohotelviki/" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a></li>
             <li class="social-icon__item"><a class="social-icon__link" href="https://zooviki.eu/" target="_blank"><ion-icon name="link-outline"></ion-icon></a></li>
